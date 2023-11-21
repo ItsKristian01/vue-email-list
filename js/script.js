2f300d3a40ce96939eea63137e58ecd4da80b811
ItsKristian01 <kristianhana11@gmail.com>
2023-11-21 15:10:20 +0100

    add: console log of one email

--- js/script.js
+++ js/script.js
@@ -3,9 +3,16 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
+        emailList: [],
     };
   },
   created() {
+    axios
+      .get("https://flynn.boolean.careers/exercises/api/random/mail")
+      .then((resp) => {
+        console.log(resp);
+        this.emailList = resp.data.response;
+      });
   },
   methods: {
   },
