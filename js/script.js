6fd30074099e887b45b9dbfc4cd596208c1943b5
ItsKristian01 <kristianhana11@gmail.com>
2023-11-22 15:11:54 +0100

    updated code

--- js/script.js
+++ js/script.js
@@ -4,20 +4,26 @@ createApp({
   data() {
     return {
       emailList: [],
+      isLoading: false
     };
   },
   created() {},
   methods: {
     generateEmails() {
-      for (let i = 10; i > this.emailList.length; i--) {
+      this.emailList = [];
+      this.isLoading = true;
+      for (let i = 0; i < 10; i++) {
         axios
           .get("https://flynn.boolean.careers/exercises/api/random/mail")
           .then((resp) => {
-           //console.log(resp);
+            console.log(resp);
             this.emailList.push(resp.data.response);
-            console.log(this.emailList);
+            if(this.emailList.length === 10) {
+              this.isLoading = false;
+            }
           });
       }
+      console.log(this.emailList);
     },
   },
 }).mount("#app");
